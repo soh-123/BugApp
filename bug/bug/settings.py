68a9109fb7e7281dd2c5891a64db93c704851854
sohier <sohier.lotfy.els@gmail.com>
2023-10-04 15:40:57 +0400

    creating bug model

--- bug/bug/settings.py
+++ bug/bug/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bug',
+
 ]
 
 MIDDLEWARE = [
@@ -105,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Dubai'
 
 USE_I18N = True
 
